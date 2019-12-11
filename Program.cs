31ac1fef4b44b789f7035fa515a53b5b51cb487f
roshangurung <r.gurung1992@gmail.com>
2019-12-10 19:15:44 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public delegate double Conversion(double from);////In this C# Program we are reading feet using ‘feet’ variable. Delegates are used to pass methods as arguments to other methods. After passing the arguments the values are converted to the required form. Print the inches of the feet using delegate.////
+class DelegateDemo
+{
+    public static double FeetToInches(double feet)
+    {
+        return feet * 12;
+    }
+
+    static void Main()
+    {
+
+        Conversion doConversion = new Conversion(FeetToInches);
+        Console.Write("Enter Feet:  ");
+        double feet = Double.Parse(Console.ReadLine());
+        double inches = doConversion(feet);
+        Console.WriteLine("\n{0} Feet = {1} Inches.\n", feet, inches);
+        Console.ReadLine();
+    }
+}
+////Output:
+///Enter Feet:  50
+////50 Feet = 600 Inches.
